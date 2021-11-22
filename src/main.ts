95caaa5b3f55fbe75fb583c5d651a6e672fd7786
康营营 <yykang@huohuaschool.com>
2021-11-22 16:42:05 +0800

    add for: 全局引用 antd design 与 自定义拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+/*全局引用*/
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import interceptors from '@/plugins/Interceptors';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(Antd)
+    .use(interceptors)
+    .mount('#app');
